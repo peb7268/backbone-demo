14607a0aaff65b5810f2db31486a88deac417938
Paul Barrick <paul@imperativedesign.net>
2013-05-22 11:52:01 -0400

    added mock functionality for the screener question booting

--- src/js/views/categories.js
+++ src/js/views/categories.js
@@ -2,12 +2,34 @@ define(function(require){
 	var Survey 		= require('models/survey'), 
 		app 		= require('app'),
 		PageView 	= require('views/pageview'), 
-		markdown 	= require('text!templates/categories.html');
+		markdown 	= require('text!templates/categories.html'),
+		selected,
+		nextPage;
 		app.page.categories = {};
 
 	var	view = app.page.categories.view = PageView.extend({
 		model: new Survey({categories: [ 'Current Events', 'Food &amp; Drinking', 'Sports Technology', 'Leisure &amp; Entertainment', 'U.S. Politics', 'Foreign Affairs', 'Business &amp; Economics']}),
-		template: _.template(require('text!templates/categories.html'))
+		template: _.template(require('text!templates/categories.html')),
+
+		events: {
+			'click input[type="checkbox"]' : 'captureCategories',
+			'click .next': 'advanceOrBoot'
+		},
+
+		captureCategories: function(e){
+			selected = [];
+			selected.push(this.$el.find('ul li input:checked').val());
+		},
+		advanceOrBoot: function(e){	
+			nextPage = $(e.target).attr('href');
+			e.preventDefault();
+			console.log(selected);
+			if(selected[0] === 'U.S. Politics') {
+				window.location = nextPage;
+			} else {
+				console.log('Thank you for wanting to take this survey. You arent good enough though.')
+			}
+		}
 	});
 
 	return view;
