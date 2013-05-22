f8ea2a5177f2c243923f94e2364de46a3059e0e4
Paul Barrick <paul@imperativedesign.net>
2013-05-22 11:15:24 -0400

    updated tests, made them more specific and focused.

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,12 +1,29 @@
 require_relative 'spec_helper.rb'
 
-describe '1 should equal 1' do 
-	it 'testing the testing' do
+describe 'Testing Pages Have correct markup' do 
+	it 'Testing intro page' do
 		visit '/'
 		page.should have_content('Get Paid for Having FUN!!')
+	end
+
+	it 'Testing categories page' do
+		visit '/#categories'
+		page.should have_content('Topic Categories')
+	end
+
+	it 'Testing background page' do
+		visit '/#background'
+		page.should have_content('Background')
+	end
+
+	it 'Testing pages can navigate' do
+		visit '/'
+		page.should have_content('Get Paid for Having FUN!!')
+		
 		click_link 'Next'
 		page.should have_content('Topic Categories')
 		page.should have_content('Current Events')
+		
 		click_link 'Next'
 		page.should have_content('Background')
 	end
