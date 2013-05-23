300afb2b6422907e32acb603e417dd222697de95
Paul Barrick <paul@imperativedesign.net>
2013-05-23 16:20:51 -0400

    integrating jasmine

---
+++ spec/unit_tests/all.js
@@ -0,0 +1,7 @@
+describe("The tests are working", function(){
+	it('expect it to have Backbone and jQuery', function(){
+		//console.log(Backbone);
+		expect(Backbone).toBeDefined();
+		expect($).toBeDefined();
+	});
+});
