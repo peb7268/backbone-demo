5635bfbac60ee99eba2fa0c29aca029f7b3f7a5a
Paul Barrick <paul@imperativedesign.net>
2013-05-22 21:15:44 -0400

    updated testing

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,6 +1,20 @@
 require_relative 'spec_helper.rb'
 
 describe 'Testing Pages Have correct markup' do 
+	it 'Testing pages can navigate' do
+		visit '/'
+		page.should have_content('Get Paid for Having FUN!!')
+		
+		click_link 'Next'
+		page.should have_content('Topic Categories')
+		page.should have_content('Current Events')
+		page.check('U.S. Politics')
+		page.check('Foreign Affairs')
+
+		click_link 'Next'
+		page.should have_content('Background')
+	end
+	
 	it 'Testing intro page' do
 		visit '/'
 		page.should have_content('Get Paid for Having FUN!!')
@@ -16,15 +30,20 @@ describe 'Testing Pages Have correct markup' do
 		page.should have_content('Background')
 	end
 
-	it 'Testing pages can navigate' do
-		visit '/'
-		page.should have_content('Get Paid for Having FUN!!')
-		
-		click_link 'Next'
+	it 'Test that the category page can advance when you click the correct screener questions' do
+		visit '/#categories'
 		page.should have_content('Topic Categories')
 		page.should have_content('Current Events')
-		
+		page.check('U.S. Politics')
+		page.check('Foreign Affairs')
+	end
+
+	it 'Test that the category page does not advance if you dont fufill the screener criteria' do
+		visit '/#categories'
+		page.check('Current Events')
+		page.check('Food & Drinking')
+
 		click_link 'Next'
-		page.should have_content('Background')
+		page.should_not have_content('Background')
 	end
 end
--- src/js/views/categories.js
+++ src/js/views/categories.js
@@ -3,32 +3,61 @@ define(function(require){
 		app 		= require('app'),
 		PageView 	= require('views/pageview'), 
 		markdown 	= require('text!templates/categories.html'),
-		selected,
 		nextPage;
 		app.page.categories = {};
 
 	var	view = app.page.categories.view = PageView.extend({
-		model: new Survey({categories: [ 'Current Events', 'Food &amp; Drinking', 'Sports Technology', 'Leisure &amp; Entertainment', 'U.S. Politics', 'Foreign Affairs', 'Business &amp; Economics']}),
+		model: new Survey({ advancementThreshold: 2, categories: [ 'Current Events', 'Food &amp; Drinking', 'Sports Technology', 'Leisure &amp; Entertainment', 'U.S. Politics', 'Foreign Affairs', 'Business &amp; Economics'], allowed: ['U.S. Politics', 'Foreign Affairs'] }),
 		template: _.template(require('text!templates/categories.html')),
 
 		events: {
-			'click input[type="checkbox"]' : 'captureCategories',
 			'click .next': 'advanceOrBoot'
 		},
 
 		captureCategories: function(e){
-			selected = [];
-			selected.push(this.$el.find('ul li input:checked').val());
+			var selected	= [],
+			$boxes = this.$el.find('ul li input[type="checkbox"]:checked');
+
+			_.each($boxes, function(elem, idx, list){
+				if($(list[idx]).is(':checked')){
+					selected.push($(list[idx]).val());
+				}
+			});
+			return selected;
+		},
+		getAllowedCategories: function(){
+			return this.model.get('allowed');
+		},
+		getAdvancementThreshold: function(){
+			return this.model.get('advancementThreshold');
+		},
+		meetsCritereaToAdvance: function(selected, allowed, threshold){
+			var criteria = [];
+			_.each(selected, function(selected_elem, index, list){
+				_.each(allowed, function(allowed_elem, index, list){
+					if(selected_elem === allowed_elem){
+						criteria.push(allowed_elem);
+					}
+				});
+			});
+			var matchesFound = (criteria.length >= threshold) ? true : false;
+			return matchesFound;
 		},
 		advanceOrBoot: function(e){	
-			nextPage = $(e.target).attr('href');
 			e.preventDefault();
-			console.log(selected);
-			if(selected[0] === 'U.S. Politics') {
-				window.location = nextPage;
-			} else {
-				console.log('Thank you for wanting to take this survey. You arent good enough though.')
-			}
+
+			//refactor and pass these members in 
+			var selected 		= this.captureCategories(),
+				allowed  		= this.getAllowedCategories(), 
+				threshold 		= this.getAdvancementThreshold(),
+				readyToAdvance 	= this.meetsCritereaToAdvance(selected, allowed, threshold),
+				nextPage 		= $(e.target).attr('href');
+		
+				if(readyToAdvance) {
+					window.location = nextPage;
+				} else {
+					//console.log('Thank you for wanting to take this survey. You arent good enough though.')
+				}
 		}
 	});
 
