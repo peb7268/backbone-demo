c519f1fd9f2c9641354e89e87d0a144b04d2cfbc
Paul Barrick <paul@imperativedesign.net>
2013-05-23 11:21:53 -0400

    updating tests

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,52 +1,56 @@
 require_relative 'spec_helper.rb'
 
-describe 'Pages can navigate correctly' do
-	it 'Testing pages can navigate' do
-		visit '/'
-		page.should have_content('Get Paid for Having FUN!!')
-		click_link 'Next'
+describe 'ICE Testing' do
+	include Capybara::DSL
+	
+	describe 'Pages can navigate correctly' do
+		it 'Testing pages can navigate' do
+			visit '/'
+			page.should have_content('Get Paid for Having FUN!!')
+			click_link 'Next'
 
-		page.should have_content('Topic Categories')
-		page.should have_content('Current Events')
-		page.check('U.S. Politics')
-		page.check('Foreign Affairs')
-		page.check('Business & Economics')
-		click_link 'Next'
-		
-		page.should have_content('Background')
-	end
-end
+			page.should have_content('Topic Categories')
+			page.should have_content('Current Events')
+			page.check('U.S. Politics')
+			page.check('Foreign Affairs')
+			page.check('Business & Economics')
+			click_link 'Next'
 
-describe 'Testing Pages Have correct markup' do 
-	it 'Testing intro page' do
-		visit '/'
-		page.should have_content('Get Paid for Having FUN!!')
+			page.should have_content('Background')
+		end
 	end
 
-	it 'Testing categories page' do
-		visit '/#categories'
-		page.should have_content('Topic Categories')
-	end
+	describe 'Testing Pages Have correct markup' do 
+		it 'Testing intro page' do
+			visit '/'
+			page.should have_content('Get Paid for Having FUN!!')
+		end
 
-	it 'Testing background page' do
-		visit '/#background'
-		page.should have_content('Background')
-	end
+		it 'Testing categories page' do
+			visit '/#categories'
+			page.should have_content('Topic Categories')
+		end
 
-	it 'Test that the category page can advance when you click the correct screener questions' do
-		visit '/#categories'
-		page.should have_content('Topic Categories')
-		page.should have_content('Current Events')
-		page.check('U.S. Politics')
-		page.check('Foreign Affairs')
-	end
+		it 'Testing background page' do
+			visit '/#background'
+			page.should have_content('Background')
+		end
 
-	it 'Test that the category page does not advance if you dont fufill the screener criteria' do
-		visit '/#categories'
-		page.check('Current Events')
-		page.check('Food & Drinking')
+		it 'Test that the category page can advance when you click the correct screener questions' do
+			visit '/#categories'
+			page.should have_content('Topic Categories')
+			page.should have_content('Current Events')
+			page.check('U.S. Politics')
+			page.check('Foreign Affairs')
+		end
 
-		click_link 'Next'
-		page.should_not have_content('Background')
+		it 'Test that the category page does not advance if you dont fufill the screener criteria' do
+			visit '/#categories'
+			page.check('Current Events')
+			page.check('Food & Drinking')
+
+			click_link 'Next'
+			page.should_not have_content('Background')
+		end
 	end
 end
